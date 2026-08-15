1a704f79fe17734a8222008894064296a7eb84c4
Yadin Nou <nouyadin@gmail.com>
2026-08-15 11:54:58 +1000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
-
+import { Button } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
 import "./App.css";
 
 function App() {
-  return <></>;
+  toast.warning("Show your messages");
+  return (
+    <>
+      <h1>Hellow world</h1>
+      <Button variant="danger">Primary</Button>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
