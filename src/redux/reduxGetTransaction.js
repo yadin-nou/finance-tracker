fd9ee4f378d11e3464939f9212f704afed88caf7
Yadin Nou <nouyadin@gmail.com>
2026-08-31 08:08:12 +1000

    modiel redux

--- src/redux/reduxGetTransaction.js
+++ src/redux/reduxGetTransaction.js
@@ -2,21 +2,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { getTransactionAxios } from "../axiosHelper/axiosConnection";
 import { getTransactionRedux } from "./transactionSlicer";
 
-const dispatch = useDispatch();
+export const ReduxTransaction = () => {
+  const dispatch = useDispatch();
 
-export const transData = useSelector((state) => state.storeTransData.transData);
+  const transData = useSelector((state) => state.storeTransData.transData);
 
-export const getTranctions = async () => {
-  try {
-    const { status, data } = await getTransactionAxios();
-    status === "success" && dispatch(getTransactionRedux(data));
-  } catch (error) {
-    console.log(error);
-  }
+  const getTranctions = async () => {
+    try {
+      const { status, data } = await getTransactionAxios();
+      status === "success" && dispatch(getTransactionRedux(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return { transData, getTranctions };
 };
-
-//   export const getTranctions= async()=>{
-//                 dispatch(getTransactionRedux({
-//                     await getTranctionsAx()
-//                 }))
-//   }
