0d247b059cb08d5a644fe86a1b66c1c684c8598e
Yadin Nou <nouyadin@gmail.com>
2026-09-01 23:22:57 +1000

    redux on dashboard added

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -5,21 +5,26 @@ export const userContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState({});
-  const [transData, setTransData] = useState([]);
+  // const [transData, setTransData] = useState([]);
 
-  const getTranctions = async () => {
-    try {
-      const { status, data } = await getTransactionAxios();
-      status === "success" && setTransData(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const getTranctions = async () => {
+  //   try {
+  //     const { status, data } = await getTransactionAxios();
+  //     status === "success" && setTransData(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
+  // return (
+  //   <userContext.Provider
+  //     value={{ user, setUser, transData, setTransData, getTranctions }}
+  //   >
+  //     {children}
+  //   </userContext.Provider>
+  // );
   return (
-    <userContext.Provider
-      value={{ user, setUser, transData, setTransData, getTranctions }}
-    >
+    <userContext.Provider value={{ user, setUser }}>
       {children}
     </userContext.Provider>
   );
